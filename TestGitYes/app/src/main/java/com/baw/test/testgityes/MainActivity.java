ab89a31cf527b2c8acfb3b973f11c96af18b84ff
wangfei <cjsswf@126.com>
2016-07-22 15:40:01 +0800

    这是第一次创建代码仓库

---
+++ TestGitYes/app/src/main/java/com/baw/test/testgityes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.baw.test.testgityes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        新建的第一个项目
+    }
+}
